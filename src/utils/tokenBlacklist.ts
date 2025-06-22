0fab6b965b06f14b5f496f8f06ce3e62888c4b07
Igor Miladinovic <miladinovic87@gmail.com>
2025-06-22 22:01:33 +0200

    added print of the entire current blacklist

--- src/utils/tokenBlacklist.ts
+++ src/utils/tokenBlacklist.ts
@@ -3,33 +3,36 @@
 import { IUserToken } from '../models/userModel';
 
 interface TokenBlacklistMap {
-        [key: string]: number;
+	[key: string]: number;
 }
 
 const tokenBlacklist: TokenBlacklistMap = {};
 
 export const addTokenToBlacklist = (token: IUserToken) => {
-        if (!token.id || token.iat === undefined || token.exp === undefined) {
-                return;
-        }
-        const key = `${token.id}-${token.iat}`;
-        tokenBlacklist[key] = token.exp * 1000; // store expiration in ms
+	if (!token.id || token.iat === undefined || token.exp === undefined) {
+		return;
+	}
+	const key = `${token.id}-${token.iat}`;
+	tokenBlacklist[key] = token.exp * 1000; // store expiration in ms
+	console.log(`Token added to blacklist: ${key}, expires at ${tokenBlacklist[key]}`);
+	//entire list of tokens
+	console.log('Current token blacklist:', tokenBlacklist);
 };
 
 export const isTokenBlacklisted = (token: IUserToken): boolean => {
-        if (!token.id || token.iat === undefined) {
-                return false;
-        }
-        const key = `${token.id}-${token.iat}`;
-        const expiry = tokenBlacklist[key];
-        if (!expiry) {
-                return false;
-        }
-        if (expiry <= Date.now()) {
-                delete tokenBlacklist[key];
-                return false;
-        }
-        return true;
+	if (!token.id || token.iat === undefined) {
+		return false;
+	}
+	const key = `${token.id}-${token.iat}`;
+	const expiry = tokenBlacklist[key];
+	if (!expiry) {
+		return false;
+	}
+	if (expiry <= Date.now()) {
+		delete tokenBlacklist[key];
+		return false;
+	}
+	return true;
 };
 
 export default tokenBlacklist;
